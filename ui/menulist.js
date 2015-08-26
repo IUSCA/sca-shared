b5ff8cfdd2df3cf980fc5d8b3bd3f12799970588
Soichi Hayashi <hayashis@soichi7.ppa.iu.edu>
2015-08-26 01:42:36 +0000

    made menulist.js to find its own path

--- ui/menulist.js
+++ ui/menulist.js
@@ -1,10 +1,19 @@
 
+//http://stackoverflow.com/questions/21103724/angular-directive-templateurl-relative-to-js-file
+//find the fullpath of "this" script.
+//idea here is that, the executing script file will always be the last on in the scripts array
+//(TODO - I need to test this on different browser)
+function dirname() {
+    var scripts = document.getElementsByTagName("script")
+    return scripts[scripts.length-1].src;
+}
+
 app.directive('scaMenulist', function(appconf) {
   return {
     restrict: 'E',
     transclude: true,
     scope: {menu: '=', active: '='},
-    templateUrl: 'menulist.html',
+    templateUrl: dirname().replace('menulist.js', 'menulist.html')
     /*
     link: function (scope, element) {
         scope.name = 'Jeff';
